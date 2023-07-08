488008048f3da67652bd2616ffa940a208e8a01f
Jora Troosh <tensorush@gmail.com>
2023-07-08 21:00:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## :lizard: :crescent_moon: **zig lua**
+
+[![CI][ci-shield]][ci-url]
+[![License][license-shield]][license-url]
+
+### Zig build of the [Lua repository](https://github.com/lua/lua) created by [Roberto Ierusalimschy](https://github.com/roberto-ieru).
+
+#### :rocket: Usage
+
+1. Add `lua` as a dependency in your `build.zig.zon`.
+
+    <details>
+
+    <summary><code>build.zig.zon</code> example</summary>
+
+    ```zig
+    .{
+        .name = "<name_of_your_package>",
+        .version = "<version_of_your_package>",
+        .dependencies = .{
+            .lua = .{
+                .url = "https://github.com/tensorush/zig-lua/archive/<git_tag_or_commit_hash>.tar.gz",
+                .hash = "<package_hash>",
+            },
+        },
+    }
+    ```
+
+    Set `<package_hash>` to `12200000000000000000000000000000000000000000000000000000000000000000`, and Zig will provide the correct found value in an error message.
+
+    </details>
+
+2. Add `lua` as a module in your `build.zig`.
+
+    <details>
+
+    <summary><code>build.zig</code> example</summary>
+
+    ```zig
+    const lua = b.dependency("lua", .{});
+    exe.addModule("lua", lua.module("lua"));
+    ```
+
+    </details>
+
+<!-- MARKDOWN LINKS -->
+
+[ci-shield]: https://img.shields.io/github/actions/workflow/status/tensorush/zig-lua/ci.yaml?branch=main&style=for-the-badge&logo=github&label=CI&labelColor=black
+[ci-url]: https://github.com/tensorush/zig-lua/blob/main/.github/workflows/ci.yaml
+[license-shield]: https://img.shields.io/github/license/tensorush/zig-lua.svg?style=for-the-badge&labelColor=black
+[license-url]: https://github.com/tensorush/zig-lua/blob/main/LICENSE.md
---
+++ build.zig
@@ -0,0 +1,154 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) std.zig.system.NativeTargetInfo.DetectError!void {
+    const target = b.standardTargetOptions(.{});
+    const version = .{ .major = 5, .minor = 4, .patch = 6 };
+    const target_info = try std.zig.system.NativeTargetInfo.detect(target);
+
+    // Library
+    const lib_step = b.step("lib", "Install library");
+
+    const lib_flags = FLAGS ++ .{switch (target_info.target.os.tag) {
+        .linux => "-DLUA_USE_LINUX",
+        .macos => "-DLUA_USE_MACOSX",
+        .windows => "-DLUA_USE_WINDOWS",
+        .ios => "-DLUA_USE_IOS",
+        else => "-DLUA_USE_POSIX",
+    }};
+
+    const lib = b.addStaticLibrary(.{
+        .name = "lua",
+        .version = version,
+        .target = target,
+        .optimize = .ReleaseSafe,
+        .link_libc = true,
+    });
+    lib.addCSourceFiles(&(CORE_FILES ++ LIB_FILES), &lib_flags);
+
+    const lib_install = b.addInstallArtifact(lib);
+    lib_step.dependOn(&lib_install.step);
+    b.default_step.dependOn(lib_step);
+
+    // Lua interpreter
+    const lua_step = b.step("lua", "Install Lua interpreter");
+
+    const lua = b.addExecutable(.{
+        .name = "lua",
+        .version = version,
+        .target = target,
+        .optimize = .ReleaseFast,
+        .link_libc = true,
+    });
+    lua.addCSourceFiles(&(.{"lua/lua.c"} ++ CORE_FILES ++ LIB_FILES), &lib_flags);
+
+    const lua_install = b.addInstallArtifact(lua);
+    lua_step.dependOn(&lua_install.step);
+    b.default_step.dependOn(lua_step);
+
+    // Tests
+    const tests_step = b.step("test", "Run tests");
+    tests_step.dependOn(&lua_install.step);
+
+    const test_flags = .{ "-Wall", "-std=gnu99", "-O2" };
+
+    inline for (TEST_LIB_FILES) |TEST_LIB_FILE| {
+        var name = std.fs.path.stem(TEST_LIB_FILE)[3..];
+        if (name.len > 1 and name[1] == '2') {
+            name = "2-v2";
+        }
+        const test_lib = b.addSharedLibrary(.{
+            .name = name,
+            .target = target,
+            .optimize = .Debug,
+            .link_libc = true,
+        });
+        test_lib.addCSourceFiles(&(CORE_FILES ++ LIB_FILES ++ .{TEST_LIB_FILE}), &test_flags);
+        if (test_lib.name.len > 1) {
+            if (test_lib.name[0] == '1') {
+                test_lib.addCSourceFile(TEST_LIB_FILES[0], &test_flags);
+            } else if (test_lib.name[1] == '1') {
+                test_lib.addCSourceFile(TEST_LIB_FILES[1], &test_flags);
+            }
+        }
+        test_lib.addIncludePath("lua");
+        test_lib.force_pic = true;
+
+        const test_lib_install = b.addInstallArtifact(test_lib);
+        tests_step.dependOn(&test_lib_install.step);
+    }
+
+    b.default_step.dependOn(tests_step);
+}
+
+const FLAGS = .{
+    "-Wall",
+    "-O2",
+    "-Wfatal-errors",
+    "-Wextra",
+    "-Wshadow",
+    "-Wundef",
+    "-Wwrite-strings",
+    "-Wredundant-decls",
+    "-Wdisabled-optimization",
+    "-Wdouble-promotion",
+    "-Wmissing-declarations",
+    "-Wdeclaration-after-statement",
+    "-Wmissing-prototypes",
+    "-Wnested-externs",
+    "-Wstrict-prototypes",
+    "-Wc++-compat",
+    "-Wold-style-definition",
+    "-std=c99",
+    "-fno-stack-protector",
+    "-fno-common",
+    "-march=native",
+    "-Wlogical-op",
+    "-Wno-aggressive-loop-optimizations",
+};
+
+const CORE_FILES = .{
+    "lua/lapi.c",
+    "lua/lcode.c",
+    "lua/lctype.c",
+    "lua/ldebug.c",
+    "lua/ldo.c",
+    "lua/ldump.c",
+    "lua/lfunc.c",
+    "lua/lgc.c",
+    "lua/llex.c",
+    "lua/lmem.c",
+    "lua/lobject.c",
+    "lua/lopcodes.c",
+    "lua/lparser.c",
+    "lua/lstate.c",
+    "lua/lstring.c",
+    "lua/ltable.c",
+    "lua/ltests.c",
+    "lua/ltm.c",
+    "lua/lundump.c",
+    "lua/lvm.c",
+    "lua/lzio.c",
+};
+
+const LIB_FILES = .{
+    "lua/lauxlib.c",
+    "lua/lbaselib.c",
+    "lua/lcorolib.c",
+    "lua/ldblib.c",
+    "lua/linit.c",
+    "lua/liolib.c",
+    "lua/lmathlib.c",
+    "lua/loadlib.c",
+    "lua/loslib.c",
+    "lua/lstrlib.c",
+    "lua/ltablib.c",
+    "lua/lutf8lib.c",
+};
+
+const TEST_LIB_FILES = .{
+    "lua/testes/libs/lib1.c",
+    "lua/testes/libs/lib2.c",
+    "lua/testes/libs/lib11.c",
+    "lua/testes/libs/lib21.c",
+    "lua/testes/libs/lib22.c",
+};
